a6ca70cd45d08a5cbe2a9f45bed38e0cbdc39ff4
paktua <arifw.affandi@yahoo.com>
2026-05-04 11:09:15 +0700

    fix service worker for pwa

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,28 @@
+importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js");
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCod6Tj0RRHezf-fW7Sj0IyKCV6unYeX9w",
+  authDomain: "kuy-bl-c4e17.firebaseapp.com",
+  projectId: "kuy-bl-c4e17",
+  storageBucket: "kuy-bl-c4e17.firebasestorage.app",
+  messagingSenderId: "192103480532",
+  appId: "1:192103480532:web:d388079075f29b8044cf41",
+});
+
+const messaging = firebase.messaging();
+
 self.addEventListener("install", () => {
   self.skipWaiting();
 });
 
 self.addEventListener("activate", () => {
-  console.log("Service Worker active");
+  console.log("SW active");
+});
+
+messaging.onBackgroundMessage((payload) => {
+  self.registration.showNotification(payload.notification.title, {
+    body: payload.notification.body,
+    icon: "/icon-192.png",
+  });
 });
